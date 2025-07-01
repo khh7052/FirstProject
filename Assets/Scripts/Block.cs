972a1c62ffc74e448ad4a8ce3bce2e275357d32a
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-01 12:29:53 +0900

    블럭 스크립트 추가

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5.0f;
+
+    bool isClicked = false;
+
+    public float xMin = -2.3f;
+    public float xMax = 2.3f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isClicked)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            if (transform.position.x < xMin || transform.position.x > xMax)
+                speed *= -1;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            isClicked = true;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
