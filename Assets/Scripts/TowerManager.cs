a5c601ca487b037acd5ba0c35e15936d2e4f756d
broccoli1962 <aq0011@naver.com>
2025-07-01 12:24:09 +0900

    TowerManager
    TowerManager

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -13,14 +13,6 @@ public class TowerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            BlockDown(block);
-        }
-    }
-
-    void BlockDown(GameObject nowBlock)
-    {
-        transform.position += Vector3.down * speed;
+        
     }
 }
