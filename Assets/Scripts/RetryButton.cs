b383b75a3e24d8f1c1d6d71c33341ef9716d3da1
김인빈 <leafpuppy1106@gmail.com>
2025-07-01 10:36:30 +0900

    change music

--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class RetryButton : MonoBehaviour
 {
+    public AudioClip startSceneBgmClip; // Inspector에서 시작화면 BGM 연결
+
     // 씬 호출용 버튼 함수
     public void LoadScene(string sceneName)
     {
+        // 씬 이동 전에 시작화면 BGM으로 전환
+        if (AudioManager.Instance != null && startSceneBgmClip != null)
+        {
+            AudioManager.Instance.PlayMusic(startSceneBgmClip);
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 
