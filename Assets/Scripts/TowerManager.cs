d4870ca84919c66a01692393feb40795e56248c6
broccoli1962 <aq0011@naver.com>
2025-07-01 15:37:11 +0900

    Spawn Block Position
    Spawn Block Position

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -14,6 +14,7 @@ public class TowerManager : MonoBehaviour
     [Header("block")]
     public List<GameObject> block = new();
     public bool blockDown = false;
+    public Transform nowBlock;
 
     [Header("Game Over")]
     public static bool isGameOver = false;
@@ -53,7 +54,8 @@ public class TowerManager : MonoBehaviour
     void CreateBlock()
     {
         int rand = Random.Range(0, block.Count);
-        Instantiate(block[rand], new Vector3(block[rand].transform.position.x, 3.5f, block[rand].transform.position.z), Quaternion.identity);
+        Instantiate(block[rand], new Vector3(block[rand].transform.position.x, Camera.main.GetComponent<CameraFollow2D>().transform.position.y + 4.5f, block[rand].transform.position.z), Quaternion.identity);
+        
     }
 
     // 점수 추가
